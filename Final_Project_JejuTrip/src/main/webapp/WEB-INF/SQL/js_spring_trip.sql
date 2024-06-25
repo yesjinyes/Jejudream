425e2e14bfc5f185a2ef0495f6fd0b10e0166491
kudi03628 <153245910+kudi03628@users.noreply.github.com>
2024-06-25 13:54:12 +0900

    개인별 sql파일 생성

---
+++ Final_Project_JejuTrip/src/main/webapp/WEB-INF/SQL/js_spring_trip.sql
@@ -0,0 +1,12 @@
+show user;
+-- USER이(가) "SYS"입니다.
+
+-- 이제 부터 오라클 계정생성시 계정명앞에 c## 붙이지 않고 생성하도록 하겠다.
+alter session set "_ORACLE_SCRIPT"=true;
+-- Session이(가) 변경되었습니다.
+
+create user final_orauser2 identified by gclass default tablespace users;
+-- User SEMI_ORAUSER2이(가) 생성되었습니다.
+
+grant connect, resource, create view, unlimited tablespace to final_orauser2;
+-- Grant을(를) 성공했습니다.
