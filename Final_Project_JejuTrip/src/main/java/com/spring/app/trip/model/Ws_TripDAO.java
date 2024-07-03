4fda1282c11b5aa2b3fa93c8e2a62de38de6f9ce
DevJeongWS <163420205+DevJeongWS@users.noreply.github.com>
2024-07-03 18:26:19 +0900

    [우석] 숙소 등록 요청 처리 완료

--- Final_Project_JejuTrip/src/main/java/com/spring/app/trip/model/Ws_TripDAO.java
+++ Final_Project_JejuTrip/src/main/java/com/spring/app/trip/model/Ws_TripDAO.java
@@ -12,7 +12,8 @@ public interface Ws_TripDAO {
 	int companyIdCheck(String companyid);// 가입하려는 아이디가 존재하는 아이디인지 체크하는 메소드
 	int companyEmailCheck(String encrypt);// 가입하려는 기업 이메일이 존재하는 이메일인지 사용가능한 이메일인지 확인하는 메소드
 	int registerHotelEnd(LodgingVO lodgingvo);// === 데이터 베이스에 등록하려는 숙소 정보 insert 하기 === // 
-	List<LodgingVO> select_lodgingvo(String choice_status);	// 숙소 등록을 신청한 업체중 선택한 카테고리에 해당하는 모든 업체들 불러오기
+	List<LodgingVO> select_lodgingvo(Map<String, String> paraMap);	// 숙소 등록을 신청한 업체중 선택한 카테고리에 해당하는 모든 업체들 불러오기
 	int screeningRegisterEnd(Map<String, String> paraMap);// 관리자가 숙소 등록 요청에 답한대로 DB를 업데이트 시켜준다.
+	int getTotalCount(String choice_status);// 총 게시물 건수(totalCount)
 
 }
