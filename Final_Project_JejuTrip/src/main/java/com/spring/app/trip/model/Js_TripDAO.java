b833c4a9ffa60dd042b1a65c0bc5ac443d86a027
kudi03628 <153245910+kudi03628@users.noreply.github.com>
2024-07-05 15:13:40 +0900

    0705 정수 숙소리스트 저장용

--- Final_Project_JejuTrip/src/main/java/com/spring/app/trip/model/Js_TripDAO.java
+++ Final_Project_JejuTrip/src/main/java/com/spring/app/trip/model/Js_TripDAO.java
@@ -1,12 +1,16 @@
 package com.spring.app.trip.model;
 
 import java.util.List;
+import java.util.Map;
 
 import com.spring.app.trip.domain.LodgingVO;
-import com.spring.app.trip.domain.PlayVO;
+
 
 public interface Js_TripDAO {
 
-	List<LodgingVO> lodgingList();
+	List<Map<String, String>> lodgingList(Map<String, Object> paraMap);
+
+	// 숙소리스트에서 조건에 따른 숙소 개수 구해오기
+	int getLodgingTotalCount(Map<String, Object> paraMap);
 
 }
